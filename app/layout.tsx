1bcddaa078298538de972852bc6bd59ca166652c
PilarEve <PilarEve@users.noreply.github.com>
2026-05-06 17:36:31 -0300

    feat: initialize root layout with Geist fonts and base HTML structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,8 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="es"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
